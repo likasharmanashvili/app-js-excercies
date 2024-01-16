9a31cc2e89b723b915f3a372b0cf8983c0dfc32e
lika <l.sharmanashvili@ssu.edu.g>
2024-01-16 23:26:27 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+//Declare a variable myNumber and assign it any numeric value using let.
+//Declare a constant variable PI and assign it the value of Pi (3.14159) using const.
+//Print the values of both variables to the console.
+//Attempt to reassign a new value to PI (even though it's declared as a constant) and observe the result.
+//Declare another variable myString and assign it any string value using let.
+//Print the value of myString to the console.
+//Reassign a new string value to myString and print the updated value.
+
+let myNumber = 42;
+
+const PI = 3.14159;
+console.log("myNumber:", myNumber);
+console.log("PI:", PI);
+let myString = "Hello, World!";
+console.log("myString:", myString);
+myString = "Updated String";
+console.log("Updated myString:", myString);
